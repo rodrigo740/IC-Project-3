68290285f9246beadfe6de26de0025d3a13b8de9
luciasousa <luciamsousa00@ua.pt>
2022-01-24 21:47:20 +0000

    local and global entropy

--- teste_fcm.cpp
+++ teste_fcm.cpp
@@ -2,9 +2,9 @@
 
 int main(){
 
-    fcm fcm1 = fcm(3,0,"texts/teste.txt");
+    fcm fcm4 = fcm(3,0,"texts/sample_texts/portugues.txt");
+    fcm fcm1 = fcm(3,0,"texts/teste2.txt");
     fcm fcm2 = fcm(3,0,"texts/sample_texts/Japanese-Lipsum.txt");
-    fcm fcm3 = fcm(3,0,"texts/japanese-lipsum_filtered.txt");
-    fcm fcm4 = fcm(3,0,"texts/lusiadas_filtered.txt");
+    fcm fcm3 = fcm(3,0,"texts/polishB2_filtered.txt");
     return 0;
 }
