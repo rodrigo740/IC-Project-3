19ed49b0eee8b3fea6278d5e466e76fdbc9827dd
luciasousa <luciamsousa00@ua.pt>
2022-01-29 19:42:06 +0000

    test lang

--- test_lang.cpp
+++ test_lang.cpp
@@ -3,23 +3,11 @@
 int main(int argc, char *argv[]) { 
 
     if(argc != 5) {
-        cout << "Usage: ./lang.o <modelLanguage> <text> <k> <alpha>" << endl;
+        cout << "Usage: ./test_lang.o <modelLanguage> <text> <k> <alpha>" << endl;
         return -1;
     }
 
-    cout << "Processing model language: " << argv[1] << "\nProcessing sample text: " << argv[2] << endl;
-
-
-    string file_lang = argv[1]; //ficheiro que representa linguagem
-    string file_text = argv[2]; //ficheiro para analisar
-    int k = stoi(argv[3]);
-    double alpha = stod(argv[4]);
-    Fcm fcm_lang = Fcm(k,alpha,file_lang);
-    Fcm fcm_text = Fcm(k,alpha,file_text);
-    float h_lang = fcm_lang.doFCM();
-    float h_text = fcm_text.doFCM();
-
-    cout << "entropy lang: " << h_lang << endl;
-    cout << "entropy text: " << h_text << endl;
+    lang l = lang(argv[1], argv[2], stoi(argv[3]), stod(argv[4]));
+    cout << "Amount of information: "<< l.doLang()<< endl;
     return 0;
 }
