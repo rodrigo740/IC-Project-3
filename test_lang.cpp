722a85a170e6cfbfa91ad528490772934f6462ad
luciasousa <luciamsousa00@ua.pt>
2022-01-29 20:01:01 +0000

    test_lang usage example

--- test_lang.cpp
+++ test_lang.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) { 
 
     if(argc != 5) {
-        cout << "Usage: ./test_lang.o <modelLanguage> <text> <k> <alpha>" << endl;
+        cout << "Usage: ./test_lang.o <modelLanguage> <text> <k> <alpha> \nExample: ./test_lang.o lang_models/portuguese.txt texts/test.txt 3 0.1" << endl;
         return -1;
     }
 
